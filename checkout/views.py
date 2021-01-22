95e86bef803af71a1dc1ce0e8df7d7352525c71d
Ryan Lovell <rjl89@live.co.uk>
2021-01-22 12:34:42 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': "pk_test_51ICNUDAKqzi40Ijpw0YaQM3BtxeeoOXEYTSYMKJelAnxz4MoqNXMXybJ88mw79oDR35eNl9llBXLddg2VN5zZYSd00Tza8Oo0i",
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
